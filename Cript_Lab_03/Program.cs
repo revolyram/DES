9ffc3ebb4ff672bf2fe0bd34044f0edf7cfabbef
Revolyram <revolyram@gmail.com>
2019-03-21 18:22:47 +0300

    Добавьте файлы проекта.

---
+++ Cript_Lab_03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cript_Lab_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
